be4065d6a3989bdacb0a9b178ff37266ce5f1f39
devMRibeiro <michael.douglas2020@hotmail.com>
2026-09-18 01:18:37 -0300

    refactor(config): improve logging formatter and parameter handling

--- src/main/java/br/com/devmribeiro/taskapi/config/Log.java
+++ src/main/java/br/com/devmribeiro/taskapi/config/Log.java
@@ -1,5 +1,8 @@
 package br.com.devmribeiro.taskapi.config;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Level;
@@ -20,31 +23,68 @@ public class Log {
 		handler.setLevel(Level.ALL);
 
 		handler.setFormatter(new Formatter() {
-			@Override
-			public String format(LogRecord record) {
 
-				return String.format("%1$tF %1$tT [%2$s] %3$s%n", new java.util.Date(record.getMillis()),
-						record.getLevel().getName(), record.getMessage());
-			}
+		    @Override
+		    public String format(LogRecord record) {
+
+		        String message = record.getMessage();
+
+		        Object[] params = record.getParameters();
+
+		        if (params != null) {
+		            for (int i = 0; i < params.length; i++) {
+		                message = message.replace(
+		                    "{" + i + "}",
+		                    String.valueOf(params[i])
+		                );
+		            }
+		        }
+
+		        StringBuilder output = new StringBuilder();
+
+		        output.append(String.format(
+		            "%1$tF %1$tT [%2$s] %3$s%n",
+		            new Date(record.getMillis()),
+		            record.getLevel().getName(),
+		            message
+		        ));
+
+		        Throwable throwable = record.getThrown();
+
+		        if (throwable != null) {
+		            StringWriter writer = new StringWriter();
+		            PrintWriter printer = new PrintWriter(writer);
+
+		            throwable.printStackTrace(printer);
+
+		            output.append(writer);
+		        }
+
+		        return output.toString();
+		    }
 		});
 
 		log.addHandler(handler);
 		log.setLevel(Level.INFO);
 	}
 	
-	public static void i(Object o) {
-		log.log(Level.INFO, "{0}", o);
+	public static void i(Object... params) {
+		log.log(Level.INFO, "", params);
 	}
-	
-	public static void w(Object o) {
-		log.log(Level.WARNING, "{0}", o);
+
+	public static void i(String message, Object... params) {
+	    log.log(Level.INFO, message, params);
 	}
-	
-	public static void e(Object o) {
-		log.log(Level.SEVERE, "{0}", o);
+
+	public static void w(String message, Object... params) {
+	    log.log(Level.WARNING, message, params);
 	}
-	
+
+	public static void e(String message, Object... params) {
+	    log.log(Level.SEVERE, message, params);
+	}
+
 	public static void e(String message, Throwable t) {
-		log.log(Level.SEVERE, message, t);
+	    log.log(Level.SEVERE, message, t);
 	}
 }
